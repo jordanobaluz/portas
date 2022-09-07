c7baacb2d32ddadd958e5be5895ed863cf331b6e
JordanoBaluz <jordanolb95@hotmail.com>
2022-09-07 11:19:08 -0300

    refatorado chamada do componente para definir com propriedades omitida ou não

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,8 +4,10 @@ import Presente from "../components/Presente";
 export default function Home() {
   return (
     <div style={{ display: "flex" }}>
+      {/* pode ser passado definindo ou não a propriedades true e false */}
       <Porta selecionada={true} />
       <Porta selecionada={false} />
+      <Porta selecionada />
       <Porta />
 
     </div>
