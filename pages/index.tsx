e044859ef4b7a9515dbdf51faa6dceb45c4b5bce
JordanoBaluz <jordanolb95@hotmail.com>
2022-09-07 17:27:12 -0300

    refatorado nome selecionada para portaComPresente, modificado index para não ser mais a tela do jogo, alterado caminho para uma url dinamica que recebe os parametros para montagem do jogo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,8 @@ import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2))
-
-  function renderizarPorta() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta}
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
-    })
-  }
-
   return (
-    <div style={{ display: "flex" }}>
-      {renderizarPorta()}
-    </div>
+    <h1>Tela de inicio do jogo</h1>
   )
 }
 
---
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -0,0 +1,41 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import Porta from "../../../components/Porta"
+import { atualizarPortas, criarPortas } from "../../../functions/portas"
+import styles from "../../../styles/Jogo.module.css"
+
+export default function jogo() {
+    //router pega os parametros passados pela URL
+    const router = useRouter()
+    const [portas, setPortas] = useState([])
+
+    //após ser montado vazio, será detectado os valores da url e chamado novamene
+    useEffect(() => {
+        //recebe o valor passado por URL, converte para number
+        const portas = +router.query.portas
+        const temPresente = +router.query.temPresente
+
+        setPortas(criarPortas(portas, temPresente))
+    }, [router?.query])
+
+
+    function renderizarPorta() {
+        return portas.map(porta => {
+            return <Porta key={porta.numero} value={porta}
+                onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
+        })
+    }
+    return (
+        <div id={styles.jogo}>
+            <div className={styles.portas}>
+                {renderizarPorta()}
+            </div>
+            <div className={styles.botoes}>
+                <Link href="/">
+                    <button>Voltar</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
